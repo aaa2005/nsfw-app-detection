1a082521f04917c556c593140b8b6df9cacd3a75
aaa2005 <71556505+aaa2005@users.noreply.github.com>
2025-05-05 13:41:06 +0100

    Update MyAccessibilityService.java

--- app/src/main/java/com/aaa2005/nsfwad/MyAccessibilityService.java
+++ app/src/main/java/com/aaa2005/nsfwad/MyAccessibilityService.java
@@ -301,6 +301,16 @@ public class MyAccessibilityService extends AccessibilityService {
             }
         }
     }
+
+    public class BootReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+                // Do something when boot is completed
+                Log.d("BootReceiver", "Device booted successfully!");
+            }
+        }
+    }
     
 }
 
